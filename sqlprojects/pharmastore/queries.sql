a1184d037f539cb509cca8ac47a6ff470fd6df0a
sundaymi <sundaymii21@gmail.com>
2026-05-06 10:09:01 +0300

    sql file updated with new queries

--- sqlprojects/pharmastore/queries.sql
+++ sqlprojects/pharmastore/queries.sql
@@ -124,3 +124,118 @@ JOIN `orders` o ON oi.`ordersid` = o.`orderid`
 GROUP BY d.`name`, YEAR(o.`orderdate`)
 ORDER BY order_year, total_sales DESC;
 
+
+-- Top Customers 
+-- select count(*) from users;
+SELECT o.`userid`, 
+	CONCAT(u.`firstname`,' ', u.`lastname`) as customer_name,
+	sum(o.ordcost) as total_spend
+FROM `users` u 	
+JOIN `orders` o ON u.`ID` = o.`userid`
+group by o.userid , customer_name 
+order by total_spend desc 
+limit 5; 
+
+
+-- Most recent order a customer has made
+select * from (
+	select o.* ,
+	row_number() over(partition by o.`userid` order by o.`orderdate`desc) as rn
+	from orders o 
+) t 
+where rn = 1;
+
+
+-- top 3 orders per customer 
+select * from 
+    (select o.*,
+    rank() over(partition by userid) as rnk
+    from orders o) x
+where x.rnk < 4;
+
+
+
+-- Customers with no orders 
+select u.* 
+from users u
+left join orders o on u.ID = o.userid 
+where o.userid is null;
+
+
+-- Total cost per customer
+select u.ID, CONCAT(u.`firstname`,' ', u.`lastname`) as customer_name, sum(o.ordcost) as total_spend
+from orders o  
+join users u on u.ID = o.userid 
+group by u.ID , customer_name; 
+
+
+-- Customers who made more than 3 orders
+select u.ID, CONCAT(u.`firstname`,' ', u.`lastname`) as customer_name, COUNT(*) as order_count
+from orders o 
+join users u on u.ID = o.userid 
+group by o.userid 
+having count(*) > 3;
+
+-- Second biggest amount of an order
+select distinct o.ordcost
+from orders o 
+order by o.ordcost desc 
+limit 1 offset 1;
+
+-- with window function
+SELECT ordcost
+FROM (
+    SELECT ordcost,
+           DENSE_RANK() OVER (ORDER BY ordcost DESC) AS rnk
+    FROM orders 
+) t
+WHERE rnk = 2;
+
+
+
+-- Orders per day
+select o.orderdate, COUNT(*) as total_orders 
+from orders o 
+group by date(o.orderdate)
+order by o.orderdate;
+
+-- Top 3 customers according to their total spent
+select u.ID, CONCAT(u.`firstname`,' ', u.`lastname`) as customer_name, SUM(o.ordcost) as total_spent 
+from orders o 
+join users u on u.ID = o.userid 
+group by u.ID, customer_name   
+order by total_spent desc 
+limit 3;
+
+
+-- Orders which total amount surpass the average (11s to execute)
+select * 
+from orders o 
+where o.ordcost > (
+	select avg(o.ordcost) from orders o2 
+); 
+
+
+-- Duplicates 
+select o.userid, o.orderdate 
+from orders o 
+group by o.userid, o.orderdate 
+having COUNT(*) > 1;
+
+-- Cumulative sum per customer
+select o.userid , o.orderdate , o.ordcost,
+SUM(ordcost) over(partition by o.userid  order by o.orderdate) as running_total
+from orders o ;
+
+
+-- index
+create index idx_user on users(lastname);
+SHOW INDEXES FROM users;
+DROP INDEX idx_users ON users;
+ALTER INDEX idx_users ON users REBUILD;
+EXEC sp_rename 'idx_users', 'idx_new_user', 'INDEX';
+
+
+
+
+
